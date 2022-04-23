87a79db86a3f75539dc7b2384be3ead3aadd8241
yzlwjt614 <1075684990@qq.com>
2022-04-23 19:00:45 +0800

    未知问题错误

--- 登录系统/登录系统/登录系统.cpp
+++ 登录系统/登录系统/登录系统.cpp
@@ -1,7 +1,22 @@
 #include<stdio.h>
+#include<conio.h>
+
+void inpsw(char psw[]);//ÃÜÂëÊäÈë·À¿ú
+
 
 int main()
 {
-
+	char psw[20];
+	inpsw(psw);
 	return 0;
+}
+
+void inpsw(char psw[])//ÃÜÂëÊäÈë·À¿ú
+{
+	int i;
+	for (i = 0; i < 6; i++)
+	{
+		psw[i] = getch();
+		putchar('*');
+	}
 }
