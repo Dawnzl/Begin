28ed0dff456fc5c0a87a311f209e7924e927f443
yzlwjt614 <1075684990@qq.com>
2022-05-22 10:26:40 +0800

    作业

---
+++ 杨政霖 第二次作业/杨政霖 第二次作业/杨政霖第二次作业.cpp
@@ -0,0 +1,335 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+#include<stdbool.h>
+
+#define ROW 4
+
+void GetNum(int arr[ROW][ROW])
+{
+	int n = 2;
+	if (rand() % 5 == 0)
+		n = 4;
+
+	int row = rand() % ROW;
+	int col = rand() % ROW;
+
+	while (arr[row][col] != 0)//坐标冲突则找"下一个"
+	{
+		col = col + 1;
+		if (col == ROW)//需要转到"下一行"的第一个
+		{
+			row = row + 1;//error
+			if (row == ROW)//已经是最后一行,则变为第一行
+				row = 0;
+			col = 0;
+		}
+	}
+	arr[row][col] = n;
+}
+
+//输出二维数组的值
+void Show(int arr[ROW][ROW])
+{
+	system("cls");
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < ROW; j++)
+		{
+			printf("%-5d", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+//开始游戏
+void Start(int arr[ROW][ROW])
+{
+	for (int i = 0; i < 2; i++)//产生两个数字
+	{
+		GetNum(arr);
+	}
+	Show(arr);
+}
+
+//获取方向.左->1,上->2,右->3,下->4
+int GetButton()
+{
+	int ch;
+	while (1)
+	{
+		if (_kbhit())//有按键
+		{
+			ch = _getch();
+			if (ch == 224)
+			{
+				ch = _getch();
+				if (ch == 75)//左
+					return 1;
+				else if (ch == 72)//上
+					return 2;
+				else if (ch == 77)//右
+					return 3;
+				else if (ch == 80)//下
+					return 4;
+			}
+		}
+	}
+	return -1;
+}
+
+//向左合并和移动数据
+bool MergeLeft(int arr[ROW][ROW])
+{
+	//合并数据
+	int index1;//第一个数字的下标
+	int index2;//第二个数字的下标
+	for (int i = 0; i < ROW; i++)
+	{
+		index1 = -1;//无效下标
+		index2 = -1;//无效下标
+		for (int j = 0; j < ROW; j++)
+		{
+			if (index1 == -1 && arr[i][j] != 0)//找到第一个需要的值
+				index1 = j;
+			else if (index2 == -1 && arr[i][j] != 0)//找到第二个需要的值
+			{
+				index2 = j;
+				if (arr[i][index1] == arr[i][index2])//合并
+				{
+					arr[i][index1] *= 2;
+					arr[i][index2] = 0;
+				}
+				else//第一个值和第二个值不同
+				{
+					index1 = index2;
+					index2 = -1;
+				}
+			}
+		}
+	}
+	//移动数据
+	for (int i = 0; i < ROW; i++)
+	{
+		index1 = -1;
+		for (int j = 0; j < ROW; j++)
+		{
+			if (index1 == -1 && arr[i][j] == 0)
+				index1 = j;
+			else if (index1 != -1 && arr[i][j] != 0)
+			{
+				arr[i][index1] = arr[i][j];
+				arr[i][j] = 0;
+				index1++;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool MergeRight(int arr[ROW][ROW])
+{
+	//合并数据
+	int index1;//第一个数字的下标
+	int index2;//第二个数字的下标
+	for (int i = ROW - 1; i >= 0; i--)
+	{
+		index1 = -1;//无效下标
+		index2 = -1;//无效下标
+		for (int j = ROW - 1; j >= 0; j--)
+		{
+			if (index1 == -1 && arr[i][j] != 0)//找到第一个需要的值
+				index1 = j;
+			else if (index2 == -1 && arr[i][j] != 0)//找到第二个需要的值
+			{
+				index2 = j;
+				if (arr[i][index1] == arr[i][index2])//合并
+				{
+					arr[i][index1] *= 2;
+					arr[i][index2] = 0;
+				}
+				else//第一个值和第二个值不同
+				{
+					index1 = index2;
+					index2 = -1;
+				}
+			}
+		}
+	}
+	//移动数据
+	for (int i = ROW - 1; i >= 0; i--)
+	{
+		index1 = -1;
+		for (int j = ROW - 1; j >= 0; j--)
+		{
+			if (index1 == -1 && arr[i][j] == 0)
+				index1 = j;
+			else if (index1 != -1 && arr[i][j] != 0)
+			{
+				arr[i][index1] = arr[i][j];
+				arr[i][j] = 0;
+				index1++;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool MergeUp(int arr[ROW][ROW])
+{
+	int indey1;//第一个数字的下标
+	int indey2;//第二个数字的下标
+	for (int j = 0; j < ROW; j++)
+	{
+		indey1 = -1;//无效下标
+		indey2 = -1;//无效下标
+		for (int i = 0; i < ROW; i++)
+		{
+			if (indey1 == -1 && arr[i][j] != 0)//找到第一个需要的值
+				indey1 = i;
+			else if (indey2 == -1 && arr[i][j] != 0)//找到第二个需要的值
+			{
+				indey2 = i;
+				if (arr[indey1][j] == arr[indey2][j])//合并
+				{
+					arr[indey1][j] *= 2;
+					arr[indey2][j] = 0;
+				}
+				else//第一个值和第二个值不同
+				{
+					indey1 = indey2;
+					indey2 = -1;
+				}
+			}
+		}
+	}
+	//移动数据
+	for (int j = 0; j < ROW; j++)
+	{
+		indey1 = -1;
+		for (int i = 0; i < ROW; i++)
+		{
+			if (indey1 == -1 && arr[i][j] == 0)
+				indey1 = i;
+			else if (indey1 != -1 && arr[i][j] != 0)
+			{
+				arr[indey1][j] = arr[i][j];
+				arr[i][j] = 0;
+				indey1++;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool MergeDown(int arr[ROW][ROW])
+{
+	int indey1;//第一个数字的下标
+	int indey2;//第二个数字的下标
+	for (int j = ROW - 1; j >= 0; j--)
+	{
+		indey1 = -1;//无效下标
+		indey2 = -1;//无效下标
+		for (int i = ROW - 1; i >= 0; i--)
+		{
+			if (indey1 == -1 && arr[i][j] != 0)//找到第一个需要的值
+				indey1 = i;
+			else if (indey2 == -1 && arr[i][j] != 0)//找到第二个需要的值
+			{
+				indey2 = i;
+				if (arr[indey1][j] == arr[indey2][j])//合并
+				{
+					arr[indey1][j] *= 2;
+					arr[indey2][j] = 0;
+				}
+				else//第一个值和第二个值不同
+				{
+					indey1 = indey2;
+					indey2 = -1;
+				}
+			}
+		}
+	}
+	//移动数据
+	for (int j = ROW - 1; j >= 0; j--)
+	{
+		indey1 = -1;
+		for (int i = ROW - 1; i >= 0; i--)
+		{
+			if (indey1 == -1 && arr[i][j] == 0)
+				indey1 = i;
+			else if (indey1 != -1 && arr[i][j] != 0)
+			{
+				arr[indey1][j] = arr[i][j];
+				arr[i][j] = 0;
+				indey1++;
+			}
+		}
+	}
+
+	return true;
+}
+
+//游戏是否结束
+bool IsOver(int arr[ROW][ROW])
+{
+	int n = 0;
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < ROW; j++)
+		{
+			if (arr[i][j] != 0)
+			{
+				n++;
+			}
+		}
+	}
+	if (n != ROW * ROW)
+		return false;
+}
+
+void Run(int arr[ROW][ROW])
+{
+	while (!IsOver(arr))
+	{
+		int n = GetButton();
+		switch (n)
+		{
+		case 1:
+			MergeLeft(arr);
+			GetNum(arr);
+			break;
+		case 2:
+			MergeUp(arr);
+			GetNum(arr);
+			break;
+		case 3:
+			MergeRight(arr);
+			GetNum(arr);
+			break;
+		case 4:
+			MergeDown(arr);
+			GetNum(arr);
+			break;
+		default:
+			break;
+		}
+		Show(arr);
+	}
+}
+
+
+int main()
+{
+	srand((unsigned int)time(NULL));//BUG修复，将时间种子种到主函数开头可以避免数字总是挨在一起
+	int arr[ROW][ROW] = { 0 };
+	Start(arr);
+	Run(arr);
+	return 0;
+}
---
+++ 杨政霖第三次作业/杨政霖第三次作业/杨政霖第三次作业.cpp
@@ -0,0 +1,409 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include <stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+#include<stdbool.h>
+#include<graphics.h>
+
+#define ROW 4
+
+void GetNum(int arr[ROW][ROW])
+{
+	int n = 2;
+	if (rand() % 5 == 0)
+		n = 4;
+
+	int row = rand() % ROW;
+	int col = rand() % ROW;
+
+	while (arr[row][col] != 0)//坐标冲突则找"下一个"
+	{
+		col = col + 1;
+		if (col == ROW)//需要转到"下一行"的第一个
+		{
+			row = row + 1;//error
+			if (row == ROW)//已经是最后一行,则变为第一行
+				row = 0;
+			col = 0;
+		}
+	}
+	arr[row][col] = n;
+}
+
+
+
+//数字转图像
+void Picture(int x, int i, int j)
+{
+	IMAGE img;
+	if (x == 0)
+	{
+		loadimage(&img, _T("0.png"), 100, 100, true);
+	}
+	if (x == 2)
+	{
+		loadimage(&img, _T("2.png"), 100, 100, true);
+	}
+	else if (x == 4)
+	{
+		loadimage(&img, _T("4.png"), 100, 100, true);
+	}
+	else if (x == 8)
+	{
+		loadimage(&img, _T("8.png"), 100, 100, true);
+	}
+	else if (x == 16)
+	{
+		loadimage(&img, _T("16.png"), 100, 100, true);
+	}
+	else if (x == 32)
+	{
+		loadimage(&img, _T("32.png"), 100, 100, true);
+	}
+	else if (x == 64)
+	{
+		loadimage(&img, _T("64.png"), 100, 100, true);
+	}
+	else if (x == 128)
+	{
+		loadimage(&img, _T("128.png"), 100, 100, true);
+	}
+	else if (x == 256)
+	{
+		loadimage(&img, _T("256.png"), 100, 100, true);
+	}
+	else if (x == 512)
+	{
+		loadimage(&img, _T("512.png"), 100, 100, true);
+	}
+	else if (x == 1024)
+	{
+		loadimage(&img, _T("1024.png"), 100, 100, true);
+	}
+	else if (x == 2048)
+	{
+		loadimage(&img, _T("2048.png"), 100, 100, true);
+	}
+	putimage(10 * (j + 1) + j * 100, 10 * (i + 1) + i * 100, &img);//图片放置位置
+}
+
+//输出二维数组的值
+void Show(int arr[ROW][ROW])
+{
+	system("cls");
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < ROW; j++)
+		{
+			//printf("%-5d", arr[i][j]);
+			Picture(arr[i][j], i, j);
+			//修改此项为画图
+		}
+		printf("\n");
+	}
+}
+
+
+//开始游戏
+void Start(int arr[ROW][ROW])
+{
+	for (int i = 0; i < 2; i++)//产生两个数字
+	{
+		GetNum(arr);
+	}
+	Show(arr);
+}
+
+//获取方向.左->1,上->2,右->3,下->4
+int GetButton()
+{
+	int ch;
+	while (1)
+	{
+		if (_kbhit())//有按键
+		{
+			ch = _getch();
+			if (ch == 224)
+			{
+				ch = _getch();
+				if (ch == 75)//左
+					return 1;
+				else if (ch == 72)//上
+					return 2;
+				else if (ch == 77)//右
+					return 3;
+				else if (ch == 80)//下
+					return 4;
+			}
+		}
+	}
+	return -1;
+}
+
+//向左合并和移动数据
+bool MergeLeft(int arr[ROW][ROW])
+{
+	//合并数据
+	int index1;//第一个数字的下标
+	int index2;//第二个数字的下标
+	for (int i = 0; i < ROW; i++)
+	{
+		index1 = -1;//无效下标
+		index2 = -1;//无效下标
+		for (int j = 0; j < ROW; j++)
+		{
+			if (index1 == -1 && arr[i][j] != 0)//找到第一个需要的值
+				index1 = j;
+			else if (index2 == -1 && arr[i][j] != 0)//找到第二个需要的值
+			{
+				index2 = j;
+				if (arr[i][index1] == arr[i][index2])//合并
+				{
+					arr[i][index1] *= 2;
+					arr[i][index2] = 0;
+				}
+				else//第一个值和第二个值不同
+				{
+					index1 = index2;
+					index2 = -1;
+				}
+			}
+		}
+	}
+	//移动数据
+	for (int i = 0; i < ROW; i++)
+	{
+		index1 = -1;
+		for (int j = 0; j < ROW; j++)
+		{
+			if (index1 == -1 && arr[i][j] == 0)
+				index1 = j;
+			else if (index1 != -1 && arr[i][j] != 0)
+			{
+				arr[i][index1] = arr[i][j];
+				arr[i][j] = 0;
+				index1++;
+			}
+		}
+	}
+
+	return true;
+}
+
+//右
+bool MergeRight(int arr[ROW][ROW])
+{
+	//合并数据
+	int index1;//第一个数字的下标
+	int index2;//第二个数字的下标
+	for (int i = ROW - 1; i >= 0; i--)
+	{
+		index1 = -1;//无效下标
+		index2 = -1;//无效下标
+		for (int j = ROW - 1; j >= 0; j--)
+		{
+			if (index1 == -1 && arr[i][j] != 0)//找到第一个需要的值
+				index1 = j;
+			else if (index2 == -1 && arr[i][j] != 0)//找到第二个需要的值
+			{
+				index2 = j;
+				if (arr[i][index1] == arr[i][index2])//合并
+				{
+					arr[i][index1] *= 2;
+					arr[i][index2] = 0;
+				}
+				else//第一个值和第二个值不同
+				{
+					index1 = index2;
+					index2 = -1;
+				}
+			}
+		}
+	}
+	//移动数据
+	for (int i = ROW - 1; i >= 0; i--)
+	{
+		index1 = -1;
+		for (int j = ROW - 1; j >= 0; j--)
+		{
+			if (index1 == -1 && arr[i][j] == 0)
+				index1 = j;
+			else if (index1 != -1 && arr[i][j] != 0)
+			{
+				arr[i][index1] = arr[i][j];
+				arr[i][j] = 0;
+				index1++;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool MergeUp(int arr[ROW][ROW])
+{
+	int indey1;//第一个数字的下标
+	int indey2;//第二个数字的下标
+	for (int j = 0; j < ROW; j++)
+	{
+		indey1 = -1;//无效下标
+		indey2 = -1;//无效下标
+		for (int i = 0; i < ROW; i++)
+		{
+			if (indey1 == -1 && arr[i][j] != 0)//找到第一个需要的值
+				indey1 = i;
+			else if (indey2 == -1 && arr[i][j] != 0)//找到第二个需要的值
+			{
+				indey2 = i;
+				if (arr[indey1][j] == arr[indey2][j])//合并
+				{
+					arr[indey1][j] *= 2;
+					arr[indey2][j] = 0;
+				}
+				else//第一个值和第二个值不同
+				{
+					indey1 = indey2;
+					indey2 = -1;
+				}
+			}
+		}
+	}
+	//移动数据
+	for (int j = 0; j < ROW; j++)
+	{
+		indey1 = -1;
+		for (int i = 0; i < ROW; i++)
+		{
+			if (indey1 == -1 && arr[i][j] == 0)
+				indey1 = i;
+			else if (indey1 != -1 && arr[i][j] != 0)
+			{
+				arr[indey1][j] = arr[i][j];
+				arr[i][j] = 0;
+				indey1++;
+			}
+		}
+	}
+
+	return true;
+}
+
+bool MergeDown(int arr[ROW][ROW])
+{
+	int indey1;//第一个数字的下标
+	int indey2;//第二个数字的下标
+	for (int j = ROW - 1; j >= 0; j--)
+	{
+		indey1 = -1;//无效下标
+		indey2 = -1;//无效下标
+		for (int i = ROW - 1; i >= 0; i--)
+		{
+			if (indey1 == -1 && arr[i][j] != 0)//找到第一个需要的值
+				indey1 = i;
+			else if (indey2 == -1 && arr[i][j] != 0)//找到第二个需要的值
+			{
+				indey2 = i;
+				if (arr[indey1][j] == arr[indey2][j])//合并
+				{
+					arr[indey1][j] *= 2;
+					arr[indey2][j] = 0;
+				}
+				else//第一个值和第二个值不同
+				{
+					indey1 = indey2;
+					indey2 = -1;
+				}
+			}
+		}
+	}
+	//移动数据
+	for (int j = ROW - 1; j >= 0; j--)
+	{
+		indey1 = -1;
+		for (int i = ROW - 1; i >= 0; i--)
+		{
+			if (indey1 == -1 && arr[i][j] == 0)
+				indey1 = i;
+			else if (indey1 != -1 && arr[i][j] != 0)
+			{
+				arr[indey1][j] = arr[i][j];
+				arr[i][j] = 0;
+				indey1++;
+			}
+		}
+	}
+
+	return true;
+}
+
+//游戏是否结束
+bool IsOver(int arr[ROW][ROW])
+{
+	for (int i = 0; i < ROW; i++)
+	{
+		for (int j = 0; j < ROW; j++)
+		{
+			if (arr[i][j] == 0)
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+
+
+
+
+void Run(int arr[ROW][ROW])
+{
+	while (!IsOver(arr))
+	{
+		int n = GetButton();
+		switch (n)
+		{
+		case 1:
+			MergeLeft(arr);
+			GetNum(arr);
+			break;
+		case 2:
+			MergeUp(arr);
+			GetNum(arr);
+			break;
+		case 3:
+			MergeRight(arr);
+			GetNum(arr);
+			break;
+		case 4:
+			MergeDown(arr);
+			GetNum(arr);
+			break;
+		default:
+			break;
+		}
+		Show(arr);
+	}
+}
+
+
+int main()
+{
+	srand((unsigned int)time(NULL));//BUG修复，将时间种子种到主函数开头可以避免数字总是挨在一起
+
+	initgraph(450, 450);	// 创建绘图窗口，大小为 450*450 像素
+	setbkcolor(0x8C815E); cleardevice();//设置背景色
+	int arr[ROW][ROW] = { 0 };
+	Start(arr);
+	Run(arr);
+
+
+	_getch();
+	closegraph();
+
+
+
+	return 0;
+}
