792e7e36461ccc3f3f0fb040bfd34b5358964824
yzlwjt614 <1075684990@qq.com>
2022-07-12 00:06:02 +0800

    草稿本

--- 数据结构/数据结构/数据结构.c
+++ 数据结构/数据结构/数据结构.c
@@ -1,17 +1,33 @@
-#include<stdio.h>
 
+//时间复杂度
+
+#include<stdio.h>
+int missNum(int* nums, int numSize)
+{
+	//直接运用数字进行计算 O(1)
+	int n = numSize + 1;
+	int ret1 = 0;
+	int i = 0;
+	for (i = 0; i < n; ++i)
+	{
+		ret1 += i;
+	}
+
+	//对未知数组中的各数进行运算 O(n)
+	int ret2 = 0;
+	int j = 0;
+	for (j = 0; j < numSize; ++j)
+	{
+		ret2 += nums[j];
+	}
+
+	return ret1 - ret2;//所以时间复杂度是O（n）
+}
 int main()
 {
 	
 	int i = 0;
-	int j = 0;
-	int z = 0;
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
-	int e = 0;
-	int f = 0;
+
 
 	return 0;
 }
