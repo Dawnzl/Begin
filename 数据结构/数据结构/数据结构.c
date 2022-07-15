e86bb111230a42fc702646b7a18b71bd26592ef2
yzlwjt614 <1075684990@qq.com>
2022-07-15 18:27:50 +0800

    数据结构

--- 数据结构/数据结构/数据结构.c
+++ 数据结构/数据结构/数据结构.c
@@ -1,42 +1,60 @@
-
-//时间复杂度
-
 #include<stdio.h>
-int missNum(int* nums, int numSize)
-{
-	//直接运用数字进行计算 O(1)
-	int n = numSize + 1;
-	int ret1 = 0;
-	int i = 0;
-	for (i = 0; i < n; ++i)
-	{
-		ret1 += i;
-	}
+#include<stdio.h>
 
-	//对未知数组中的各数进行运算 O(n)
-	int ret2 = 0;
+int main()
+{
+	//时间复杂度
+//int missNum(int* nums, int numSize)
+//{
+//	//直接运用数字进行计算 O(1)
+//	int n = numSize + 1;
+//	int ret1 = 0;
+//	int i = 0;
+//	for (i = 0; i < n; ++i)
+//	{
+//		ret1 += i;
+//	}
+//
+//	//对未知数组中的各数进行运算 O(n)
+//	int ret2 = 0;
+//	int j = 0;
+//	for (j = 0; j < numSize; ++j)
+//	{
+//		ret2 += nums[j];
+//	}
+//
+//	return ret1 - ret2;//所以时间复杂度是O（n）
+//}
+//
+//
+//int missNum2(int* nums, int numSize)
+//{
+//	int x = 0;
+//	int i = 0;
+//	for (i = 0; i < numSize + 1; ++i)
+//	{
+//		x ^= i;//与有序的数值异或
+//	}
+//	for (i = 0; i < numSize; ++i)
+//	{
+//		x ^= nums[i];//再与缺少一个数的数组进行异或
+//	}
+//	return x;
+//}
+
+	//从低到高去找ret里面一个为1的位 j
+	int ret = 0;
 	int j = 0;
-	for (j = 0; j < numSize; ++j)
+	for (j = 0; j < 32; ++j)
 	{
-		ret2 += nums[j];
+		if (ret & (1 << j))//如果成立则是找到ret中从左往右第一个为 1 的位：j 
+			break;
 	}
 
-	return ret1 - ret2;//所以时间复杂度是O（n）
+
+	return 0;
+
 }
 
 
-int missNum2(int* nums, int numSize)
-{
-	int x = 0;
-	int i = 0;
-	for (i = 0; i < numSize + 1; ++i)
-	{
-		x ^= i;//与有序的数值异或
-	}
-	for (i = 0; i < numSize; ++i)
-	{
-		x ^= nums[i];//再与缺少一个数的数组进行异或
-	}
-	return x;
-}
 
