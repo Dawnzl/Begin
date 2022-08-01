f71c6aee1ee16e95d57cefbcdcd37cb84bbe1a37
yzlwjt614 <1075684990@qq.com>
2022-08-01 16:04:45 +0800

    链表

--- Slist/Slist/test.c
+++ Slist/Slist/test.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include"slist.h"
+#include"DList.h"
+
 
 void TesSList1()
 {
@@ -64,9 +66,60 @@ void TesSList3()
 	SListPrint(plist);
 }
 
+void TestList1()
+{
+	ListNode* plist = ListInit();
+	ListPushBack(plist, 1);
+	ListPushBack(plist, 2);
+	ListPushBack(plist, 3);
+	ListPushBack(plist, 4);
+
+	ListPrint(plist);
+
+	ListPushFront(plist, 1);
+	ListPushFront(plist, 2);
+	ListPushFront(plist, 3);
+	ListPushFront(plist, 4);
+
+	ListPrint(plist);
+
+	ListPopBack(plist);
+	ListPopBack(plist);
+	ListPopBack(plist);
+
+	ListPrint(plist);
+
+	ListPopFront(plist);
+	ListPopFront(plist);
+	ListPopFront(plist);
+
+	ListPrint(plist);
+}
+
+void TestList2()
+{
+	ListNode* plist = ListInit();
+	ListPushBack(plist, 1);
+	ListPushBack(plist, 2);
+	ListPushBack(plist, 3);
+	ListPushBack(plist, 4);
+	ListPushBack(plist, 5);
+	ListPushBack(plist, 6);
+
+	ListPrint(plist);
+
+	ListNode* pos = Listfind(plist, 4);
+	if (pos)
+	{
+		ListInert(pos, 40);
+	}
+	ListPrint(plist);
+
+}
 
 int main()
 {
-	TesSList3();
+	//TesSList3();
+	TestList1();
 
 }
