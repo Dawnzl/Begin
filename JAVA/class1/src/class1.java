e56743458ac491fc7f1b0a1cf322989bf5e19823
yzlwjt614 <1075684990@qq.com>
2022-08-14 10:15:25 +0800

    java

--- JAVA/class1/src/class1.java
+++ JAVA/class1/src/class1.java
@@ -1,14 +1,26 @@
-
-class Person{
-    //属性
-    public int age;
-    public String name;
-
-    //行为
-    public void zouren
-}
-
+import java.util.Arrays;
 
 public class class1 {
+        public static void func1(int[] array) {
+        int i = 0;
+        int j = array.length - 1;
+        while (i < j) {
+            while (i < j && array[i] % 2 != 0) {
+                i++;
+            }
+            while (i < j && array[j] % 2 == 0) {
+                j--;
+            }
+            int tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+    }
 
+        public static void main(String[] args){
+        int[] array = {1, 2, 3, 4, 5, 6};
+        func1(array);
+        System.out.println(Arrays.toString(array));
+    }
 }
+
