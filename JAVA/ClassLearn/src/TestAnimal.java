5d1ec61d105f38c219c1b8f14158b5e267a81be7
yzlwjt614 <1075684990@qq.com>
2022-09-13 09:40:08 +0800

    java

--- JAVA/ClassLearn/src/TestAnimal.java
+++ JAVA/ClassLearn/src/TestAnimal.java
@@ -2,6 +2,12 @@ class Animal{
     public String name;
     public int age;
 
+    public Animal(String name,int age)//父类构造方法
+    {
+        this.name = name;
+        this.age = age;
+    }
+
     public void eat(){
         System.out.println(name+"吃饭");
     }
@@ -9,24 +15,30 @@ class Animal{
 
 class Dog1 extends Animal{
     // 傻 是狗1的特殊属性
-    public String silly;
+    public boolean silly;
+
+    //有参数要自己显性构造方法
+    public Dog1(String name,int age,boolean silly){
+        super(name,age);//必须在第一行,先帮父类部分初始化
+        this.silly = silly;
+    }
     public void houseGuard(){
         System.out.println(super.name+"正在看家护院");
         System.out.println(name+"正在看见护院");
     }
 }
 
-class Cat extends Animal{
+/*class Cat extends Animal{
     // 猫特有的行为
     public void catchMouse(){
         System.out.println(name+"抓老鼠");
     }
-}
+}*/
 
 public class TestAnimal {
     public static void main(String[] args) {
-        Dog1 dog = new Dog1();
-        Cat cat = new Cat();
+        Dog1 dog = new Dog1("xiao",18,false);
+        //Cat cat = new Cat();
 
     }
 }
