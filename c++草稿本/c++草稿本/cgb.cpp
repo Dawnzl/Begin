c6a7e6459440fb4ce14b7be7f4cb91c478b199b2
yzlwjt614 <1075684990@qq.com>
2022-09-18 10:27:00 +0800

    草稿本

--- c++草稿本/c++草稿本/cgb.cpp
+++ c++草稿本/c++草稿本/cgb.cpp
@@ -1,45 +1,73 @@
 #include<iostream>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
-struct WP {
-    int v;
-    int w;
-    double arg;//均价
-};
 
-bool cmp(WP a, WP b)
-{
-    return a.arg > b.arg;
-}
 int main()
 {
-    WP* W; // 设置物品
-    int n = 0, V = 0;
-    cin >> V >> n; // 物品个数，背包体积
-    W = new WP[n];//开n个物品空间
-    for (int i = 0; i < n; ++i)
+
+    int bagWeight, num;
+    cin >> bagWeight >> num;
+    vector<int> weight(num);
+    vector<int> value(num);
+    for (int i = 0; i < num; ++i)
     {
-        cin >> W[i].v;
+        cin >> weight[i] >> value[i];
     }
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> W[i].w;
-        W[i].arg = W[i].w / (W[i].v * 1.0);
-    }
-    sort(W, W + n, cmp);
-    int maxV = 0;
-    for (int i = 0; i < n; ++i)
-    {
-        if (W[i].v <= V)
-        {
-            V -= W[i].v;
-            maxV += W[i].w;
+
+    // 初始化
+    vector<int> dp(bagWeight + 1, 0);
+    for (int i = 0; i < weight.size(); i++)
+    { // 遍历物品
+        for (int j = bagWeight; j >= weight[i]; j--)
+        { // 遍历背包容量
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
         }
-        else
-            break;
     }
-    cout << maxV << endl;
-    return 0;
+    cout << dp[bagWeight] << endl;
+	return 0;
 }
 
+
+//struct WP {
+//    int v;
+//    int w;
+//    double arg;//均价
+//};
+//
+//bool cmp(WP a, WP b)
+//{
+//    return a.arg > b.arg;
+//}
+//int main()
+//{
+//
+//    WP* W; // 设置物品
+//    int n = 0, V = 0;
+//    cin >> V >> n; // 物品个数，背包体积
+//    W = new WP[n];//开n个物品空间
+//    for (int i = 0; i < n; ++i)
+//    {
+//        cin >> W[i].v;
+//    }
+//    for (int i = 0; i < n; ++i)
+//    {
+//        cin >> W[i].w;
+//        W[i].arg = W[i].w / (W[i].v * 1.0);
+//    }
+//    sort(W, W + n, cmp);
+//    int maxV = 0;
+//    for (int i = 0; i < n; ++i)
+//    {
+//        if (W[i].v <= V)
+//        {
+//            V -= W[i].v;
+//            maxV += W[i].w;
+//        }
+//        else
+//            break;
+//    }
+//    cout << maxV << endl;
+//    return 0;
+//}
